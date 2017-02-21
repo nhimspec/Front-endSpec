44c4b3a90370f526f15f56723be16c58e41072a1
Sonic Spec <sonicspec@Sonics-MacBook-Pro.local>
2017-02-21 10:46:45 +0700

    update

---
+++ assets/scripts/vendor/vendor.js
@@ -0,0 +1,20 @@
+//@prepros-prepend ../../vendor/jquery/dist/jquery.min.js
+//@prepros-prepend ../../vendor/bootstrap-sass/assets/javascripts/bootstrap/transition.js
+//@prepros-prepend ../../vendor/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js
+//@prepros-prepend ../../vendor/bootstrap-sass/assets/javascripts/bootstrap/modal.js
+//@prepros-prepend ../../vendor/owl.carousel/dist/owl.carousel.min.js
+//@prepros-prepend ../../vendor/isotope/isotope.pkgd.min.js
+//@prepros-prepend ../../vendor/wow/dist/wow.min.js
+
+(function() {
+  (function() {
+    'use strict';
+    var msViewportStyle;
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+      msViewportStyle = document.createElement('style');
+      msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
+      document.head.appendChild(msViewportStyle);
+    }
+  })();
+
+}).call(this);
